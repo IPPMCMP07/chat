9a7bfa78546b46085805d616fd31ee90863042df
prashantRmishra <pm7044872@gmail.com>
2019-03-08 20:18:20 +0530

    chat and collab

---
+++ collab_chat/server.js
@@ -0,0 +1,32 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io= require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname +'/index.html');
+});
+var temp="";
+io.on('connection',function(socket){
+	
+	console.log('a new connection id: '+socket.id);
+	io.emit('temp',temp);
+	
+	//io.emit('new', socket.id);
+	socket.on('chat message',function(data){
+		console.log(data);
+		io.emit('chat message', data);
+		});
+		socket.on('code',function(code){
+			console.log(code);
+			io.emit('code',code);
+			temp=code;
+		});
+		
+	socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000,'0.0.0.0', function(){
+  console.log('listening on *:3000');
+});
